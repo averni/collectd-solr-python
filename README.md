de3db0f6d4286d9833f7e85fd30239fe36e1a95e
Denis Zhdanov <denis.zhdanov@gmail.com>
2013-02-24 17:29:03 +0100

    More cleanup, preparing for actual code

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 collectd-solr-python
 ====================
 
-Python-based plugin to put various [Solr] (http://lucene.apache.org/solr/) stats to [collectd](http://collectd.org)
+Python-based plugin to put various [Solr 4.X] (http://lucene.apache.org/solr/) stats to [collectd](http://collectd.org)
 
 Data captured includes:
 
  * TODO
 
-Cores detected automatically.
+Cores are detecting automatically. XML structure
 Munin plugins from [Distilled Media Ltd.] (https://github.com/distilledmedia/munin-plugins/) and [Gasol Wu] (https://github.com/Gasol/munin-solr-plugins/)
 were used as donors.
 [powdahoud's redis-collectd-plugin] (https://github.com/powdahound/redis-collectd-plugin/) was used as template also.
 
 Install
 -------
+ 0. Place stats requestHandler in solrconfig.xml for all cores, since stats.jsp is not working since 4.x
+   <requestHandler
+     name="/admin/stats"
+     class="org.apache.solr.handler.admin.SolrInfoMBeanHandler" />
+
  1. Place collectd-solr.py in /usr/lib/collectd/plugins/python
  2. Configure the plugin (see below).
  3. Restart collectd.
@@ -34,6 +39,6 @@ Add the following to your collectd config
         Host "localhost"
         Port 8080
         URL "/solr"
-        AdminURL "/solr/admin/cores"
+        AdminURL "admin/stats?stats=true"
       </Module>
     </Plugin>
--- collectd-solr.py
+++ collectd-solr.py
@@ -37,7 +37,15 @@
 #
 
 import collectd
-
+import os, sys, urllib2
+try:
+    import xml.etree.cElementTree as etree
+except ImportError:
+    try:
+        import xml.etree.ElementTree as etree
+    except ImportError:
+        print 'python >= 2.5'
+        sys.exit()
 
 # Host to connect to. Override in config by specifying 'Host'.
 SOLR_HOST = 'localhost'
@@ -49,77 +57,69 @@ SOLR_PORT = 8080
 SOLR_URL = '/solr'
 
 # Solr Admin URL. Override in config by specifying 'SolrAdminURL'.
-SOLR_ADMIN_URL = '/solr/admin/cores'
+SOLR_ADMIN_URL = 'admin/stats?stats=true'
 
+
+def get_cores():
+    url = '%s?action=status' % SOLR_ADMIN_URL
+    f = urllib2.urlopen(url)
+    xml = etree.fromstring(f.read())
+    cores = [lst.attrib['name'].strip() for lst in xml.findall('./lst/lst')]
+    return cores
 
 def fetch_info():
-  """Connect to Solr stat page and request info"""
-  return
+    """Connect to Solr stat page and request info"""
+    return
 
 
 def configure_callback(conf):
-  """Receive configuration block"""
-  global solr_HOST, solr_PORT, VERBOSE_LOGGING
-  for node in conf.children:
-    if node.key == 'Host':
-      solr_HOST = node.values[0]
-    elif node.key == 'Port':
-      solr_PORT = int(node.values[0])
-    elif node.key == 'Verbose':
-      VERBOSE_LOGGING = bool(node.values[0])
-    else:
-      collectd.warning('collectd-solr plugin: Unknown config key: %s.'
-                       % node.key)
-  log_verbose('Configured with host=%s, port=%s' % (solr_HOST, solr_PORT))
+    """Receive configuration block"""
+    global SOLR_HOST, SOLR_PORT, SOLR_URL, SOLR_ADMIN_URL
+    for node in conf.children:
+        if node.key == 'Host':
+            SOLR_HOST = node.values[0]
+        elif node.key == 'Port':
+            SOLR_PORT = int(node.values[0])
+        if node.key == 'Host':
+            SOLR_URL = node.values[0]
+        if node.key == 'Host':
+            SOLR_ADMIN_URL = node.values[0]
+        else:
+            collectd.warning('collectd-solr plugin: Unknown config key: %s.' % node.key)
+    log_verbose('Configured: host=%s, port=%s, url=%s, admin_url=%s' % (SOLR_HOST, SOLR_PORT, SOLR_URL, SOLR_ADMIN_URL))
 
 
 def dispatch_value(info, key, type, type_instance=None):
-  """Read a key from info response data and dispatch a value"""
-  if key not in info:
-    collectd.warning('collectd-solr plugin: Info key not found: %s' % key)
-    return
+    """Read a key from info response data and dispatch a value"""
+    if key not in info:
+        collectd.warning('collectd-solr plugin: Info key not found: %s' % key)
+        return
 
-  if not type_instance:
-    type_instance = key
+    if not type_instance:
+        type_instance = key
 
-  value = int(info[key])
-  log_verbose('Sending value: %s=%s' % (type_instance, value))
+    value = int(info[key])
+    log_verbose('Sending value: %s=%s' % (type_instance, value))
 
-  val = collectd.Values(plugin='collectd-solr')
-  val.type = type
-  val.type_instance = type_instance
-  val.values = [value]
-  val.dispatch()
+    val = collectd.Values(plugin='collectd-solr')
+    val.type = type
+    val.type_instance = type_instance
+    val.values = [value]
+    val.dispatch()
 
 
 def read_callback():
-  log_verbose('Read callback called')
-  info = fetch_info()
+    log_verbose('Read callback called')
+    info = fetch_info()
 
-  if not info:
-    collectd.error('solr plugin: No info received')
-    return
+    if not info:
+        collectd.error('solr plugin: No info received')
+        return
 
-  # send high-level values
-  dispatch_value(info, 'uptime_in_seconds','gauge')
-  dispatch_value(info, 'connected_clients', 'gauge')
-  dispatch_value(info, 'connected_slaves', 'gauge')
-  dispatch_value(info, 'blocked_clients', 'gauge')
-  dispatch_value(info, 'evicted_keys', 'gauge')
-  dispatch_value(info, 'used_memory', 'bytes')
-  dispatch_value(info, 'changes_since_last_save', 'gauge')
-  dispatch_value(info, 'total_connections_received', 'counter',
-                 'connections_recieved')
-  dispatch_value(info, 'total_commands_processed', 'counter',
-                 'commands_processed')
-
-  # database and vm stats
-  for key in info:
-    if key.startswith('vm_stats_'):
-      dispatch_value(info, key, 'gauge')
-    if key.startswith('db'):
-      dispatch_value(info[key], 'keys', 'gauge', '%s-keys' % key)
-
+    # send high-level values
+    dispatch_value(info, 'test1', 'gauge')
+    dispatch_value(info, 'test2', 'bytes')
+    dispatch_value(info, 'test3', 'counter', 'values')
 
 # register callbacks
 collectd.register_config(configure_callback)
